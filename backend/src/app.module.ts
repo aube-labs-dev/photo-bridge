506b3217f0209732ff0730f2519facec9c43f4da
jonghwa.park <jonghwa.park@gmail.com>
2025-07-02 16:58:28 +0900

    submodule init

--- backend/src/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PhotoTransferGateway } from './photo-transfer.gateway';
-
-@Module({
-  imports: [],
-  controllers: [],
-  providers: [PhotoTransferGateway],
-})
-export class AppModule {}
--- frontend/next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export', // 이 설정을 추가하여 정적 빌드
-  distDir: 'out', // 빌드 결과물이 'out' 디렉토리에 생성되도록 (Nginx와 일치)
-};
-
-module.exports = nextConfig;
